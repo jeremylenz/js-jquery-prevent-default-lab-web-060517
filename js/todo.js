a03bdccd30876053c669e5604307f645a52d9d71
jeremylenz <jeremylenz@gmail.com>
2017-07-20 15:56:56 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
   // call functions here
+
+submitForm()
+
 });
 
 // define functions here
+
+function submitForm() {
+  $('form').on('submit', function(event){
+    let currentList = $('#list ol')
+    let newItem = document.createElement('li')
+    let textBox = $('input')[0]
+    let newItemText = textBox.value
+    newItem.innerHTML = newItemText
+    currentList.append(newItem);
+    textBox.value = ""
+    // event.preventDefault();
+  })
+}
